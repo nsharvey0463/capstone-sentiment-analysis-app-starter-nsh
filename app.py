974b0a01d009ad35e3e3e9d304b103f29f3d7dfc
Neil Harvey <nsharvey0463@gmail.com>
2025-06-06 16:42:11 -0700

    Fix Sentiment in app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def index():
         sentiment = vader_sentiment
         sentiment["custom model positive"] = sentiment_analysis(text)
     
-    return render_template("form.html", sentimet=sentiment)
+    return render_template("form.html", sentiment=sentiment)
 
 if __name__ == "__main__":
     app.run(debug=True)
