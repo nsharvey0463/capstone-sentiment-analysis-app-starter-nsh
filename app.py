a102df249d3e67bcc2585d72e1284c4c759cb754
Neil Harvey <nsharvey0463@gmail.com>
2025-06-06 14:27:12 -0700

    try debug code

--- app.py
+++ app.py
@@ -38,6 +38,8 @@ def index():
         text = request.form["user_text"]
         sentiment = si_analyzer.polarity_scores(text)
         sentiment['custom model positive'] = sentiment_analysis(text)
+        print("this is the sentiment")
+        print(sentiment)
         return render_template("form.html", sentiment=sentiment)
 
     return render_template('form.html')
